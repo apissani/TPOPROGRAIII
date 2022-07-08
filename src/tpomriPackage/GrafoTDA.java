5743cd912688f1b2d96bcd968103c218f9ed3bce
alep66 <63133789+apissani@users.noreply.github.com>
2022-07-08 09:28:49 -0300

    Add files via upload

---
+++ src/tpomriPackage/GrafoTDA.java
@@ -0,0 +1,11 @@
+package tpomriPackage;
+
+public interface GrafoTDA {
+
+	public void agregarVertice(int v);
+
+	public void agregarArista(int v1, int v2, int peso);
+
+	public void mostrarGrafo();
+
+}
